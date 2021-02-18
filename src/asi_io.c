39e4f49e3e2934ba0a2a37fd3578905452836715
alexkoehn <alexander-koehn@web.de>
2021-02-18 22:10:13 +0100

    Added robust PNM image header parsing

---
+++ src/asi_io.c
@@ -0,0 +1,208 @@
+#include "asi_io.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int image_init (image_type *image, int width, int height, dtype_enum dtype)
+{
+    image->width = width;
+    image->height = height;
+    image->dtype = dtype;
+
+    /* Allocate memory for integer image */
+    if (dtype == ASI_DTYPE_INT)
+    {
+        image->data = (int *) malloc(width * height * sizeof(int));
+
+        if (image->data == NULL)
+        {
+            return ASI_EXIT_FAILED_ALLOC;
+        }
+
+        return ASI_EXIT_SUCCESS; 
+    }
+
+    /* Allocate memory for double image */
+    if (dtype == ASI_DTYPE_DOUBLE)
+    {
+        image->data = (double *) malloc(width * height * sizeof(double));
+
+        if (image->data == NULL)
+        {
+            return ASI_EXIT_FAILED_ALLOC;
+        }
+
+        return ASI_EXIT_SUCCESS; 
+    }
+
+    return ASI_EXIT_INVALID_DTYPE;
+}
+
+int image_read_pnm_header(pnm_header_type *header, const char* filename)
+{
+    FILE *file;
+    char *buffer;
+
+    buffer = (char*) malloc(256 * sizeof(char));
+
+    /* Attempt to open file */
+    file = fopen(filename, "rb");
+
+    if (!file)
+    {
+        free(buffer);
+        return ASI_EXIT_FILE_NOT_FOUND;
+    }
+
+    /* Minimal header length (= PBM header without comments) */
+    header->header_length = 2;
+
+    /* Parse file type */
+    if (fgets(buffer, 256, file) == NULL)
+    {
+        free(buffer);
+        fclose(file);
+        return ASI_EXIT_INVALID_FTYPE;
+    }
+
+    /* Skip comments and empty lines */
+    while (buffer[0] == '#' || buffer[0] == ' ' || buffer[0] == '\n')
+    {
+        if (fgets(buffer, 256, file) == NULL)
+        {
+            free(buffer);
+            fclose(file);
+            return ASI_EXIT_INVALID_FTYPE;
+        }
+
+        header->header_length++;
+    }
+
+    if (buffer[0] == 'P' && buffer[1] != '\n')
+    {
+        switch (buffer[1])
+        {
+            case '1' : 
+                header->ftype = PNM_P1;
+                break;
+            case '2' :
+                header->ftype = PNM_P2;
+                break;
+            case '3' :
+                header->ftype = PNM_P3;
+                break;
+            case '4' :
+                header->ftype = PNM_P4;
+                break;
+            case '5' :
+                header->ftype = PNM_P5;
+                break;
+            case '6' :
+                header->ftype = PNM_P6;
+                break;
+            default :
+                free(buffer);
+                fclose(file);
+                return ASI_EXIT_INVALID_FTYPE;
+        }
+    }
+    else
+    {
+        fclose(file);
+        free(buffer);
+        return ASI_EXIT_INVALID_FTYPE;
+    }
+
+    /* Parse image dimensions */
+    if (fgets(buffer, 256, file) == NULL)
+    {
+        free(buffer);
+        fclose(file);
+        return ASI_EXIT_INVALID_FTYPE;
+    }
+
+    /* Skip comments and empty lines */
+    while (buffer[0] == '#' || buffer[0] == ' ' || buffer[0] == '\n')
+    {
+        if (fgets(buffer, 256, file) == NULL)
+        {
+            free(buffer);
+            fclose(file);
+            return ASI_EXIT_INVALID_FTYPE;
+        }
+
+        header->header_length++;
+    }
+
+    sscanf(buffer, "%d %d", &header->width, &header->height);
+
+    if (header->width < 1 || header->height < 1)
+    {
+        free(buffer);
+        fclose(file);
+        return ASI_EXIT_INVALID_IMG_DIM;
+
+    /* Parse value depth (only defined for .pgm) */
+    if (header->ftype == PNM_P1 || header->ftype == PNM_P4)
+    {
+        header->data_depth = 1;
+    }
+    else
+    {
+        if (fgets(buffer, 256, file) == NULL)
+        {
+            free(buffer);
+            fclose(file);
+            return ASI_EXIT_INVALID_FTYPE;
+        }
+        
+        sscanf(buffer, "%d", &header->data_depth);
+
+        if (header->data_depth <= 0)
+        {
+            free(buffer);
+            fclose(file);
+            return ASI_EXIT_INVALID_DDEPTH;
+        }
+
+        header->header_length++;
+    }
+
+    /* Skip empty lines after header */
+    while (fgets(buffer, 256, file))
+    {
+        if (buffer[0] == '#' || buffer[0] == ' ' || buffer[0] == '\n')
+        {
+            header->header_length++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+    free(buffer);
+    fclose(file);
+
+    return ASI_EXIT_SUCCESS;
+}
+
+
+int image_read_pgm (image_type *image, const char *filename)
+{
+    int ret;
+    FILE *file;
+    char buffer[256];
+    pnm_header_type header;
+
+    /* Parse PNM header */
+    ret = image_read_pnm_header(&header, filename);
+
+    if (ret != ASI_EXIT_SUCCESS)
+    {
+        return ret;
+    }
+
+
+    return ASI_EXIT_SUCCESS;
+}
+
---
+++ src/asi_io.h
@@ -0,0 +1,69 @@
+#ifndef _ASI_IO_H_
+#define _ASI_IO_H_
+
+#define ASI_EXIT_SUCCESS 1
+#define ASI_EXIT_FAILURE 0
+#define ASI_EXIT_FAILED_ALLOC 100
+#define ASI_EXIT_INVALID_DTYPE 101
+#define ASI_EXIT_INVALID_FTYPE 102
+#define ASI_EXIT_INVALID_DDEPTH 103
+#define ASI_EXIT_INVALID_IMG_DIM 104
+#define ASI_EXIT_FILE_NOT_FOUND 105
+
+/* Supported datatypes for image structs */
+typedef enum dtype
+{
+    ASI_DTYPE_INT,
+    ASI_DTYPE_DOUBLE,
+    ASI_DTYPE_INT_RGB,
+    ASI_DTYPE_DOUBLE_RGB
+} dtype_enum;
+
+typedef enum pnm_ftype
+{
+    PNM_P1,
+    PNM_P2,
+    PNM_P3,
+    PNM_P4,
+    PNM_P5,
+    PNM_P6
+} pnm_ftype_enum;
+
+typedef struct rgb_tuple
+{
+    void *r;
+    void *g;
+    void *b;
+} rgb_tuple_type;
+
+/* Image data structure */
+typedef struct image
+{
+    void *data; /* Void pointer to image data */
+    int width;  /* Image width (Number of columns) */
+    int height; /* Image height (Number of rows) */
+    dtype_enum dtype; /* Image data type */
+    int has_colour; /* Boolean to indicate colour image */
+} image_type;
+
+typedef struct pnm_header
+{
+    pnm_ftype_enum ftype;
+    int width;
+    int height;
+    int data_depth;
+    int header_length;
+} pnm_header_type;
+
+/* Allocate memory for image struct */
+int image_init (image_type *image, int width, int height, 
+        dtype_enum dtype);
+
+/* Access image data using x and y coordinates */
+void * image_at (image_type image, int i, int j);
+
+/* Import */
+int image_read_pnm_header(pnm_header_type *header, const char* filename);
+int image_read_pgm (image_type *image, const char *filename);
+
+#endif
